d0c70e7393cfbb3312aa9095a68b169927e187b7
Randy Steele <steelerx155@gmail.com>
2019-10-02 17:15:54 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,12 @@
 def second_challenge
 epic_tragedy = {
    :montague => {
-   :patriarch => { },
-   :matriarch => {},
-   :hero => {},
-   :hero_friends => []
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
    },
-      :capulet => {
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
