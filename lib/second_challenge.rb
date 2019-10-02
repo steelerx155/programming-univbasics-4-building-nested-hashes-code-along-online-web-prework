fe9c37b7ada8343cf2a1a1b27d6b259321daa6dc
Randy Steele <steelerx155@gmail.com>
2019-10-02 14:10:27 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,12 +10,11 @@ epic_tragedy = {
       ]
    },
       :capulet => { 
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
+          {},
+          {}
       ]
    }
-  }
